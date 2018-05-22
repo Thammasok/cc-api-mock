4430ea54d2659950ed7c739ddefd23dae49ef15e
Jaranchai <jaranchai.nt@hotmail.com>
2018-05-22 15:14:39 +0700

    allow origin

--- app.js
+++ app.js
@@ -24,6 +24,14 @@ app.use(expressValidator());
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  //console.log(req)
+  next();
+});
+
 app.use('/', index);
 app.use('/product', products);
 

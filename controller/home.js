78fa01d645a96861f61cd2a3365b32bd93231acf
Jaranchai <jaranchai.nt@hotmail.com>
2018-05-30 13:36:20 +0700

    change text

--- controller/home.js
+++ controller/home.js
@@ -1,7 +1,7 @@
 exports.Index = function(req, res, next) {
   return res.status(200).json({
     title: "ChomCHOB mock API",
-    description: "This is mock api service for skill test of front-end developer",
+    description: "This is mock API service for skill test of the front-end developer",
     company_info: {
       company_name: "Chomchob Group Co.,Ltd.", 
       address: "69/6 Tang Hua Pug Building (9th Floor) alley Sathon10, Nort Sathon Road, Silom, Bang Rak, Bangkok 10500",
